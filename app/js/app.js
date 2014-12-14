5df13c07f6f2e4e557c3907338dc6c8dc72eded8
Alek <agrinb@gmail.com>
2014-12-14 00:07:00 -0500

    Added preview to the form

--- app/js/app.js
+++ app/js/app.js
@@ -11,7 +11,10 @@
   .directive('bookGenres', function(){
   	return {
   		restrict: 'E',
-  		templateUrl: 'partials/book-genres.html'	
+  		templateUrl: 'partials/book-genres.html',
+  		scope: {
+        genres: '='
+      }	
   	}
   })
 
@@ -30,6 +33,7 @@
   		replace: true,
   		contorller: function(){
   			this.showForm = false;
+  			this.book = {};
   		},
   		controllerAs: 'reviewFormCtrl',
   		scope: {
